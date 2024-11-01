3c8d6c24dfba3a9442579aa36d4a98fdf388065d
LodzinhGabriel <gabrielgamerroblox@gmail.com>
2024-11-01 15:46:48 -0300

    começo 2

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 app.route("/")
 def index():
     return "<h1>Página inicial<h1>"
+
+app.route("/contato", methods=("POST", ))
+def contato():
+    return "<h1>Contato<h1>"
+
+app.route("/sobre")
+def sobre():
+    return "<h1>Sobre<h1>"
+    
+app.route("/produtos/", methods=("GET", ))
+def produtos(pesquisa = None):
+    return "<h1>Produtos<h1>"
+
+app.route("/compra/<string:produto>")
+def compra(produto = None):
+    return "<h1>Produtos<h1>"
